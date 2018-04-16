004d821d7c5cfaef96d99eed26cd7ad41167730e
redtachyon <jakub.kwiatkowski.96@gmail.com>
2018-04-16 17:45:44 +0200

    Initial commit

---
+++ tools/.ipynb_checkpoints/points_processing-checkpoint.py
@@ -0,0 +1,110 @@
+import json
+import pickle
+
+import numpy as np
+import pandas as pd
+from scipy.io import loadmat
+from tqdm import tqdm
+
+#import dataholders as dh
+import tools.utils as utils
+
+
+def prepare_points(path='../data/points.txt'):
+    """
+    Parses the output of label app to a dataframe with timestamps of all jump points, including
+    the nearest points from the original time vector, converted to milliseconds
+    """
+    
+    with open('data/points.txt') as f:
+        lines = f.readlines()
+    
+    lines = [json.loads(line.strip()) for line in lines]
+    
+    lines = list(filter(lambda x: x['mode'] == 'jump', lines)) # choose only jump points
+    
+    df = pd.DataFrame(lines)
+    
+    # Cast everything to numeric datatypes
+    df['noiseStep'] = df['noiseStep'].astype(int)
+    df['thermometer'] = df['thermometer'].astype(int)
+    df['value'] = df['value'].astype(float)
+    df['value'] = (1000 * df['value']).astype(int) # Storing timestamps in int's for easier comparing
+    
+    df.value = df.value.apply(lambda x: x + 1 if x % 10 != 0 else x) # Fix a dumb glitch
+    
+    return df
+
+def prepare_data(points_path='data/points.txt', data_path='data/data16.pickle'):
+    """
+    Reads the marked jump points and the time and temperature vectors, with time measured in milliseconds.
+    """
+    
+    df = prepare_points(points_path)
+    with open(data_path, 'rb') as f:
+        all_data = pickle.load(f)
+        
+    time, lowT, upT = all_data['time_av'], all_data['lowT_av'], all_data['upT_av']
+    time, [lowT, upT] = utils.fast_synchronize(time, lowT, upT)
+    
+    low_labels = np.in1d(time, df[df.thermometer == 0].value)
+    up_labels  = np.in1d(time, df[df.thermometer == 1].value)
+
+    return time, low_labels, up_labels, lowT, upT
+
+def smear_labels(a: np.ndarray, size: int) -> np.ndarray:
+    """
+    Gives positive labels to points within $size points of actual positive labels.
+    
+    Args:
+        a: np.array, labels array
+        size: int, how far away the labels are smeared
+    """
+    
+    pad = size # Amount of zero's to add on both sides
+    a_padded = np.concatenate((np.zeros(pad), a, np.zeros(pad)))
+    rolled = utils.rolling_window(a_padded, size*2 + 1)
+    
+    return rolled.max(1).astype(a.dtype)
+
+def generate_positive(labels, time, temperature, size=20):
+    """
+    Generates time and temperature vectors of length $size (preferably divisible by 4), containing a jump.
+    """
+    time_data        = []
+    temperature_data = []
+    
+    for i, val in tqdm(enumerate(labels), total=len(labels)):
+        window = labels[i:i+size] # size: how many points around a jump
+        
+        left   = i + (1*size//4)
+        center = i + (2*size//4)
+        right  = i + (3*size//4)
+
+        if labels[left:left+1] == 1 or labels[center:center+1] == 1 or labels[right:right+1] == 1:
+            time_data.append(time[i:i+size])
+            temperature_data.append(temperature[i:i+size])
+        
+    if time_data[-1].shape != (size,):
+        time_data        = list(filter(lambda x: x.shape == (size,),        time_data))
+        temperature_data = list(filter(lambda x: x.shape == (size,), temperature_data))
+
+    return np.array(time_data), np.array(temperature_data)
+
+def generate_negative(labels, time, temperature):
+    """
+    Generates time and temperature vectors of length 20, hopefully not containing a jump, in a safe distance from actual jumps.
+    """
+    labels = labels.copy()
+
+    time_data        = []
+    temperature_data = []
+
+    for i, val in tqdm(enumerate(labels), total=len(labels)):
+        window = labels[i:i+50]
+        if window.max() == 0 and i + 50 < len(labels):
+            time_data.append(time[i+15:i+35])
+            temperature_data.append(temperature[i+15:i+35])
+            labels[i:i+50] = 1
+    
+    return np.array(time_data), np.array(temperature_data)
---
+++ tools/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,113 @@
+import numpy as np
+import pickle
+
+
+def rolling_window(series, window):
+    """
+    Helper function for performing rolling window computations.
+    Adds an extra dimension to the array, which can then be used to do whatever you need to do on windows.
+
+    Args:
+        series: np.array, series to be unrolled
+        window: int, size of the rolling window
+
+    Returns:
+        np.array
+    """
+    shape = series.shape[:-1] + (series.shape[-1] - window + 1, window)
+    strides = series.strides + (series.strides[-1],)
+    return np.lib.stride_tricks.as_strided(series, shape=shape, strides=strides)
+
+
+def read_pickle(path):
+    """Reads an array from the path, using pickle"""
+    with open(path, "rb") as f:
+        data = pickle.load(f)
+
+    return data
+
+
+def read_data(path, low=275000, high=300000):
+    """Reads data and preformats it."""
+    data = read_pickle(path)
+
+    Y1 = data['lowT_av'].squeeze()
+    Y2 = data['upT_av'].squeeze()
+    # LWC = data['lwc1V_av']
+    X = np.arange(Y1.shape[0]) / 100.
+    # X = data['time_av'].squeeze()
+    if low is not None and high is not None:
+        X = X[low:high]
+        Y1 = Y1[low:high]
+        Y2 = Y2[low:high]
+
+    return X, Y1, Y2
+
+
+def array_range(a, low, high, ref=None):
+    """
+    Returns the array limited to values in selected range.
+    """
+    if ref is None:
+        ref = a
+    return a[np.logical_and(ref >= low, ref < high)]
+
+
+def fast_synchronize(time_uft: np.ndarray, *arrays, how='linear') -> (np.ndarray, np.ndarray):
+    """
+    Synchronizes the UFT time and record vector to conform with the ACTOS timestamps, and hopefully does so quickly.
+    """
+    
+    time_uft = rolling_window(time_uft, 2).mean(1, dtype=int)
+    narrays = [rolling_window(array, 2).mean(1) for array in arrays]
+    
+    return time_uft, narrays
+
+def slow_synchronize(time_actos: np.ndarray, time_uft: np.ndarray, *arrays, how='linear') -> (np.ndarray, np.ndarray):
+    """
+    Synchronizes the UFT time and record vector to conform with the ACTOS timestamps.
+    """
+    
+    narrays = [[] for array in arrays]
+    
+    for t in tqdm(time_uft):
+        try:
+            index = np.where(time_uft == t - 5)[0][0]
+        except IndexError:
+            continue
+            
+        for i, array in enumerate(arrays):
+            narrays[i].append((array[index] + array[index + 1])/2)
+        
+    narrays = list(map(np.array, narrays))
+    
+    return time_uft, narrays
+
+def write_report(classifier, train_data, train_labels, test_data, test_labels, keras=False):
+    """
+    Evaluates the classifier.
+    """
+    train_preds = classifier.predict(train_data)
+    test_preds = classifier.predict(test_data)
+    
+    if keras:
+        train_preds = train_preds > .5
+        test_preds = test_preds > .5
+    
+    print("Training data (less important):")
+    print(" Accuracy: %.5f\n Precision: %.5f\n Recall: %.5f\n \033[91m F1 score (class 1): %.5f\n\033[0m F1 score (class 0): %.5f" 
+          % (accuracy_score(train_labels, train_preds),
+             precision_score(train_labels, train_preds),
+             recall_score(train_labels, train_preds),
+             f1_score(train_labels, train_preds),
+             f1_score(train_labels, train_preds, pos_label=0)
+            ))
+    
+    print("Test data (more important):")
+    print(" Accuracy: %.5f\n Precision: %.5f\n Recall: %.5f\n \033[91m F1 score (class 1): %.5f\n\033[0m F1 score (class 0): %.5f" 
+          % (accuracy_score(test_labels, test_preds),
+             precision_score(test_labels, test_preds),
+             recall_score(test_labels, test_preds),
+             f1_score(test_labels, test_preds),
+             f1_score(test_labels, test_preds, pos_label=0)
+            ))
